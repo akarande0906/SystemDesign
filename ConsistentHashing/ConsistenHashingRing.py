e1295c8d99e3bffe682d247e299825eb4141e4a2
Alok Karande <102259086+akarande0906@users.noreply.github.com>
2025-11-20 22:39:45 -0800

    Update ConsistenHashingRing.py
    Corrected the handling of Virtual Nodes

--- ConsistentHashing/ConsistenHashingRing.py
+++ ConsistentHashing/ConsistenHashingRing.py
@@ -1,13 +1,14 @@
-'''
+"""
 This class implements a consistent hashing ring to distribute keys across multiple cache nodes.
 Each cache node is represented by an instance of the CacheNode class.
-'''
-# from pytreemap import TreeMap
+"""
+
 from typing import List, Optional
 from cache.CacheNode import CacheNode
 import hashlib
 import bisect
 import logging
+from collections import defaultdict  
 
 logging.basicConfig(filename='consistent_hashing.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -20,6 +21,8 @@ class ConsistentHashingRing:
         self.sorted_keys = [] # Sorted list of hash keys
         self.servers = set() # Set of servers in the ring
         self.cache_size = cache_size # Cache size for each CacheNode
+        self.virtual_node_map = {} # Map of virtual nodes to real servers
+        self.server_virtual_node_map = defaultdict(list) # Map of real servers to their virtual nodes
        
         logger.debug("Initializing Consistent Hashing Ring. Adding Servers: %s", servers)
 
@@ -40,19 +43,34 @@ class ConsistentHashingRing:
         ''' Adds a server to the hash ring with virtual nodes based on the replication factor '''
         self.servers.add(server)
         logger.debug("Adding Server: %s to the hash ring", server)
-        for i in range(self.replication_factor):
+        parent_hash_val = self._get_hash_key(f"{server}-{0}") 
+        logger.debug("Adding parent node with hash: %d for server: %s-0", parent_hash_val, server)
+        self.ring[parent_hash_val] = CacheNode(instance_no=parent_hash_val, cache_size=self.cache_size)
+
+        self.virtual_node_map[parent_hash_val] = parent_hash_val
+        self.server_virtual_node_map[parent_hash_val].append(parent_hash_val)
+
+        bisect.insort(self.sorted_keys, parent_hash_val)  
+       
+        for i in range(1, self.replication_factor):
             # Add as many virtual nodes as the replication factor
             hash_val = self._get_hash_key(f"{server}-{i}") 
             logger.debug("Adding virtual node with hash: %d for server: %s-%d", hash_val, server, i)
-            self.ring[hash_val] = CacheNode(instance_no=hash_val, cache_size=self.cache_size)
+
+            self.virtual_node_map[hash_val] = parent_hash_val
+            self.server_virtual_node_map[parent_hash_val].append(hash_val)
+
+            #self.ring[hash_val] = CacheNode(instance_no=hash_val, cache_size=self.cache_size)
             # Keeping it sorted will help in efficient lookups
             bisect.insort(self.sorted_keys, hash_val)  
 
     def remove_server(self, server: str) -> None:
         ''' Removes a server and its virtual nodes from the hash ring '''
         if server in self.servers:
-            logger.debug("Removing Server: %s from the hash ring", server)
-            for i in range(self.replication_factor):
+            # Get the parent hash value
+            parent_hash_val = self._get_hash_key(f"{server}-0")
+            logger.debug("Removing parent node with hash: %d for server: %s-0", parent_hash_val, server)
+            for i in range(1, self.replication_factor):
                 hash_val = self._get_hash_key(f"{server}-{i}")
                 logger.debug("Removing virtual node with hash: %d for server: %s-%d", hash_val, server, i)
                 # Remove from ring and sorted keys
@@ -60,6 +78,12 @@ class ConsistentHashingRing:
                 # Find the index of hash_val in sorted_keys and remove it
                 index = bisect.bisect_left(self.sorted_keys, hash_val)
                 del self.sorted_keys[index]
+
+            # Finally remove the parent node
+            del self.virtual_node_map[parent_hash_val]
+            del self.server_virtual_node_map[parent_hash_val]
+            del self.ring[parent_hash_val]
+
             self.servers.remove(server)
             return True
         else:
@@ -73,11 +97,14 @@ class ConsistentHashingRing:
         if not self.ring:
             return None
         hash_val = self._get_hash_key(key)
+        logger.debug("Hashed key: %s to hash value: %d", key, hash_val)
         # Find the index of the nearest server to the right (clockwise)
         index = bisect.bisect(self.sorted_keys, hash_val) % len(self.sorted_keys)
         if index == len(self.sorted_keys):
             index = 0  # Wrap around to the first server
-        return self.ring[self.sorted_keys[index]]  # Return the assigned CacheNode
+        virtual_node_hash = self.sorted_keys[index]
+        parent_hash = self.virtual_node_map[virtual_node_hash]
+        return self.ring[parent_hash]  # Return the assigned CacheNode
     
     def get_servers(self) -> List[dict]: 
         ''' Returns the list of servers in the hash ring for testing purposes '''
@@ -137,10 +164,12 @@ if __name__ == "__main__":
     ring.put_cache_entry("key2", "value2")
     ring.put_cache_entry("key31", "value31")
     ring.put_cache_entry("key21", "value21")
+    ring.put_cache_entry("all", "good")
     print(ring.get_cache_entry("key1"))  # Should print value1
     print(ring.get_cache_entry("key2"))  # Should print value2
     print(ring.get_cache_entry("key31"))  # Should print value31
     print(ring.get_cache_entry("key21"))  # Should print value21
+    print(ring.get_cache_entry("all"))  # Should print sum
     
 
 
