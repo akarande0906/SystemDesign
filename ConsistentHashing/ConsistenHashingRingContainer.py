1b883dc70d18a43803d06edb1340f349b3ad7969
Alok Karande <102259086+akarande0906@users.noreply.github.com>
2025-11-20 22:39:03 -0800

    Update ConsistenHashingRingContainer.py
    Updated to handle Virtual Nodes correctly

--- ConsistentHashing/ConsistenHashingRingContainer.py
+++ ConsistentHashing/ConsistenHashingRingContainer.py
@@ -1,8 +1,8 @@
-'''
+"""
 This class implements a consistent hashing ring to distribute keys across multiple cache nodes.
 Each cache node is represented by an instance of the CacheNode class.
-'''
-# from pytreemap import TreeMap
+"""
+
 from DockerHelper import CacheDockerHelper
 from cache.CacheNode import CacheNode
 from typing import List
@@ -10,6 +10,7 @@ import hashlib
 import bisect
 import logging
 import requests
+from collections import defaultdict
 
 logging.basicConfig(filename='consistent_hashing.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -25,6 +26,9 @@ class ConsistentHashingRingContainer:
         self.ring = {} # Create the Hash Ring
         self.sorted_keys = [] # Sorted list of hash keys
         self.servers = set() # Set of servers in the ring
+        self.virtual_node_map = {} # Map of virtual nodes to real servers
+        self.server_virtual_node_map = defaultdict(list) # Map of real servers to their virtual nodes
+       
         self.cache_size = cache_size # Cache size for each CacheNode
         self.cur_port = 5000  # Starting port for CacheNode instances
         self.docker_helper = CacheDockerHelper(port_base=self.cur_port)
@@ -49,22 +53,38 @@ class ConsistentHashingRingContainer:
         ''' Adds a server to the hash ring with virtual nodes based on the replication factor '''
         self.servers.add(server)
         logger.debug("Adding Server: %s to the hash ring", server)
-        for i in range(self.replication_factor):
+        container_name = f"{server}-{0}"
+        parent_hash_val = self._get_hash_key(container_name) 
+        logger.debug("Adding parent node with hash: %d for server: %s-0", parent_hash_val, server)
+
+        self.cur_port += 1
+        self.ring[parent_hash_val] = self.docker_helper.create_container(name=f'lru-cache-{server}', instance_no=parent_hash_val, cache_size=self.cache_size, port=self.cur_port)
+
+        self.virtual_node_map[parent_hash_val] = parent_hash_val
+        self.server_virtual_node_map[parent_hash_val].append(parent_hash_val)
+        
+        for i in range(1, self.replication_factor):
             # Add as many virtual nodes as the replication factor
             container_name = f"{server}-{i}"
             hash_val = self._get_hash_key(container_name) 
             logger.debug("Adding virtual node with hash: %d for server: %s-%d", hash_val, server, i)
+
+            self.virtual_node_map[hash_val] = parent_hash_val
+            self.server_virtual_node_map[parent_hash_val].append(hash_val)
+
             # self.ring[hash_val] = CacheNode(instance_no=hash_val, cache_size=self.cache_size)
-            self.cur_port += 1
-            self.ring[hash_val] = self.docker_helper.create_container(name=f'lru-cache-{container_name}', instance_no=hash_val, cache_size=self.cache_size, port=self.cur_port)
+            # self.ring[hash_val] = self.docker_helper.create_container(name=f'lru-cache-{container_name}', instance_no=hash_val, cache_size=self.cache_size, port=self.cur_port)
             # Keeping it sorted will help in efficient lookups
             bisect.insort(self.sorted_keys, hash_val)  
 
     def remove_server(self, server: str) -> None:
         ''' Removes a server and its virtual nodes from the hash ring '''
+        parent_hash_val = self._get_hash_key(f"{server}-0")
+        logger.debug("Removing parent node with hash: %d for server: %s-0", parent_hash_val, server)
+
         if server in self.servers:
             logger.debug("Removing Server: %s from the hash ring", server)
-            for i in range(self.replication_factor):
+            for i in range(1, self.replication_factor):
                 container_name = f"{server}-{i}"
                 hash_val = self._get_hash_key(container_name)
                 logger.debug("Removing virtual node with hash: %d for server: %s-%d", hash_val, server, i)
@@ -75,6 +95,12 @@ class ConsistentHashingRingContainer:
                 # Find the index of hash_val in sorted_keys and remove it
                 index = bisect.bisect_left(self.sorted_keys, hash_val)
                 del self.sorted_keys[index]
+
+            # Finally remove the parent node
+            del self.virtual_node_map[parent_hash_val]
+            del self.server_virtual_node_map[parent_hash_val]
+            del self.ring[parent_hash_val]
+
             self.servers.remove(server)
             return True
         else:
@@ -88,11 +114,14 @@ class ConsistentHashingRingContainer:
         if not self.ring:
             return None
         hash_val = self._get_hash_key(key)
+        logger.debug("Hashed key: %s to hash value: %d", key, hash_val)
         # Find the index of the nearest server to the right (clockwise)
         index = bisect.bisect(self.sorted_keys, hash_val) % len(self.sorted_keys)
         if index == len(self.sorted_keys):
             index = 0  # Wrap around to the first server
-        return self.ring[self.sorted_keys[index]]  # Return the assigned Server
+        virtual_node_hash = self.sorted_keys[index]
+        parent_hash = self.virtual_node_map[virtual_node_hash]
+        return self.ring[parent_hash]  # Return the assigned Server
     
     def get_servers(self) -> List[dict]: 
         ''' Returns the list of servers in the hash ring for testing purposes '''
@@ -157,7 +186,7 @@ class ConsistentHashingRingContainer:
 # ----- Testing -----
 
 if __name__ == "__main__":
-    ring = ConsistentHashingRingContainer(cache_size=3, servers=["server1", "server2"], replication_factor=2)
+    ring = ConsistentHashingRingContainer(cache_size=10, servers=["server1", "server2"], replication_factor=2)
     ring.put_cache_entry("key1", "value10")
     ring.put_cache_entry("key2", "value2") 
     ring.put_cache_entry("key3", "value3")
